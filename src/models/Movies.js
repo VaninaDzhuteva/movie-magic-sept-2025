c6ee6d378e9575f57baca9edf25941347dd0e053
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-10-18 14:21:44 +0300

    Add genre filter

--- src/models/Movies.js
+++ src/models/Movies.js
@@ -66,6 +66,7 @@ export default class Movie {
 
     if (filter.genre) {
       // exact match,case insensitive
+      result = result.filter(movie => movie.genre.toLowerCase === filter.genre.toLowercase());
     }
 
     if (filter.year) {
