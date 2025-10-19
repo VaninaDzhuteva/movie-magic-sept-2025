2fbd80197794e8201707cd6d85cee8e03be9593c
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-10-19 20:54:56 +0300

    Refactor search

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,13 +1,31 @@
+import { log } from "console";
 import Movie from "../models/Movie.js";
 
-async function getAll(filter) {
+function getAll(filter) {
     // const result = await Movie.find(filter).lean();
-    const result = await Movie.find(filter);
-    return result;
+    let query = Movie.find();
+
+    if (filter.title) {
+      // Partial match,case insensitive
+      query = query.find({title: { $regex: filter.title, $options: 'i'}});
+    }
+
+    if (filter.genre) {
+      // exact match,case insensitive
+      query = query.find({genre: { $regex: new RegExp(`ˆ${filter.genre}$`), $options: 'i'}});
+    }
+
+    if (filter.year) {
+      // exact match,case insensitive
+      query = query.where('year').equals(filter.year);
+    }
+
+    return query;
 }
 
 function getOne(movieId) {
-    return Movie.findOne({ _id: movieId });
+    //return Movie.findOne({ _id: movieId });
+    return Movie.findById(movieId);
 }
 
 function create(movieData) {
