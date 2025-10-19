170f266a91c9878563dc0df7c70efd943a8ff582
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-10-19 17:11:26 +0300

    Use lean to fix handlebars error

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,8 @@
 import Movie from "../models/Movie.js";
 
-function getAll(filter) {
-    return Movie.find(filter);
+async function getAll(filter) {
+    const result = await Movie.find(filter).lean();
+    return result;
 }
 
 function getOne(movieId) {
